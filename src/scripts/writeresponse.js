3ccf97f14c56a725841384b19deefe218a1f4567
louiswong18 <louiswong18@gmail.com>
2023-07-26 19:04:03 +0800

    Add files via upload

--- src/scripts/writeresponse.js
+++ src/scripts/writeresponse.js
@@ -4,6 +4,14 @@ const path = require('path');
 
 let allHolidays = {};
 
+// Define output directory
+const outputDir = path.join(__dirname, 'country holidays');
+
+// Create output directory if it doesn't exist
+if (!fs.existsSync(outputDir)){
+    fs.mkdirSync(outputDir);
+}
+
 newman.run({
     collection: require('./getHoliday.postman_collection.json'),
     environment: require('./environment.postman_environment.json'),
@@ -12,35 +20,52 @@ newman.run({
         console.error(error);
         return;
     }
-    // Assuming `name` field in the request can be used as filename
-    const filename = data.item.name.replace(/[^a-z0-9]/gi, '_').toLowerCase(); // replace non-alphanumeric characters with underscore and make it lower case
-    fs.writeFile(path.join(__dirname, `${filename}.json`), data.response.stream.toString(), function (error) {
-        if (error) { 
-            console.error(error);
-        } else {
-            console.log(`Response saved to ${filename}.json`);
 
-            let responseData = JSON.parse(data.response.stream.toString());
+  // Join the path items into a single string
+  const fullPath = data.item.request.url.path.join('/');
 
-            // Assuming the holidays are in the `items` array
-            let holidays = responseData.items.filter(item => !item.description.includes('observance')).map(item => ({
-                date: item.start.date,
-                name: item.summary,
-            }));
+  // Split the resulting string by '/'
+  const pathParts = fullPath.split('/');
 
-            allHolidays[filename] = holidays;
-        }
-    });        
-}).on('done', function (error, summary) {
-    if (error) {
+  // The calendar ID is the fourth part of the path
+  const calendarID = pathParts[3];  // 'en.bd%23holiday%40group.v.calendar.google.com' or 'en.australian%23holiday%40group.v.calendar.google.com'
+  const country_calendarID = `${data.item.name} ${decodeURIComponent(calendarID)}`;
+
+  console.log(`Calendar ID for ${data.item.name}: ${decodeURIComponent(calendarID)}`);
+
+
+  // Assuming `name` field in the request can be used as filename
+  const filename = data.item.name.replace(/[^a-z0-9]/gi, '_').toLowerCase(); // replace non-alphanumeric characters with underscore and make it lower case
+
+  // Write file to output directory
+  fs.writeFile(path.join(outputDir, `${filename}.json`), data.response.stream.toString(), function (error) {
+    if (error) { 
         console.error(error);
-        return;
+    } else {
+        console.log(`Response saved to ${filename}.json`);
+
+        let responseData = JSON.parse(data.response.stream.toString());
+
+        let holidays = responseData.items.filter(item => !item.description.includes('observance')).map(item => ({
+            date: item.start.date,
+            name: item.summary,
+        }));
+
+        allHolidays[country_calendarID] = holidays;
     }
-    fs.writeFile(path.join(__dirname, 'holidays.json'), JSON.stringify(allHolidays, null, 2), function (error) {
-        if (error) { 
-            console.error(error); 
-        } else {
-            console.log(`All holidays saved to holidays.json`);
-        }
-    });
+});        
+}).on('done', function (error) {
+  if (error) {
+      console.error(error);
+      return;
+  }
+
+  // Write allHolidays to output directory
+  fs.writeFile(path.join(outputDir, 'holidays.json'), JSON.stringify(allHolidays, null, 2), function (error) {
+      if (error) { 
+          console.error(error); 
+      } else {
+          console.log(`All holidays saved to holidays.json`);
+      }
+  });
 });
